c7581b9fb96b7e97504b5559504ab2caa5e1d5a2
Nestor Ojeda <nojeda@adasoft.es>
2022-04-20 23:36:48 +0200

    Initial commit

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import crm_model
+from . import sale_order_rev
+
---
+++ models/crm_model.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models
+
+
+# class CustomCrm(models.Model):
+#     _inherit = 'crm.lead'
